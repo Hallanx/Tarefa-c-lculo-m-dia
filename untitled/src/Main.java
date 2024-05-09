90b64ec38c19d1adb8a2ded30a6d46f82e90fde7
ALAN VAZ CARDOSO <145813724+Hallanx@users.noreply.github.com>
2024-05-09 12:39:10 -0300

    Add files via upload

---
+++ untitled/src/Main.java
@@ -0,0 +1,14 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+            long nota1 = 10;
+            long nota2 = 8;
+            long nota3 = 1;
+            long nota4 = 5;
+
+            long resultado = nota1 + nota2 + nota3 + nota4;
+
+            System.out.println(resultado / 4);
+        }
+    }
